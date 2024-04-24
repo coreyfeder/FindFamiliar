8e5459dadc3ae0e92fe2bb5167a1ad6d4b70b57b
Corey Feder <corey.feder@gmail.com>
2024-04-23 21:51:48 -0400

    use populated documents for roster

--- server/src/models/users.mjs
+++ server/src/models/users.mjs
@@ -1,10 +1,31 @@
 import mongoose from "mongoose";
-import familiarSchema from "./familiars.mjs";
 
 const userSchema = new mongoose.Schema({
-    ipid: { type: String, trim: true, required: true, index: text, },
+    // IF making a fake ID for fake auth
+    // id: { type: String, trim: true, required: true, index: text, },
     name: { type: String, trim: true, required: true, },
-    familiars: [ familiarSchema ],
+    roster: [
+        new mongoose.Schema({
+            name: String,
+            familiar: {
+                type: mongoose.SchemaTypes.ObjectId,
+                ref: "Familiar",
+                required: true,
+            },
+            note: {
+                type: String,
+                maxlength: 500,
+            },
+            // images: [ /* ??? */ ],
+            // as BinData?
+            // use GridFS? https://www.mongodb.com/docs/manual/core/gridfs/
+            // use mongoose-gridstore plugin?
+            //      https://www.npmjs.com/package/mongoose-gridstore
+            // use mongoose-gm plugin for images?
+            //      https://www.npmjs.com/package/mongoose-gm
+            // use multer? https://github.com/expressjs/multer#usage
+        })
+    ],
 });
 
 export default mongoose.model(schema=userSchema, collection="users");
