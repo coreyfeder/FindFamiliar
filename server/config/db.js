228bec697a6068096ded6e96e62b0f0108e3642f
Corey Feder <corey.feder@gmail.com>
2024-04-23 22:18:48 -0400

    clean up debris in DB connection file

--- server/config/db.js
+++ server/config/db.js
@@ -1,25 +1,17 @@
 // One-and-done imports
 if (process.env.MODE != "production" ) {
-    // TODO: confirm this does the same as `require("dotenv").config();`
     import "dotenv/config";
+    if (process.env.MDB_DATABASE === undefined) {
+        import dotenv from 'dotenv';
+        dotenv.config({path: "../.env"});
+    }
 }
 
 import mongoose from "mongoose";
 
-let str, add, first
-str = "qwer";
-// add = ["asdf", "zxcv"]
-add = []
-first = true;
-for (const s of add) {
-    let news = (first ? "?" : "&" ) + s;
-    console.log(`adding "${news}"`);
-    str += news;
-    first = false;
-}
-console.log(str)
-
 // NEXT: construct or declare the connection string
+// This _should_ use a server API key
+//      but...Mongoose can only use username:password credentials??
 // DRAFT
 // this section is entirely overengineered,
 // but for now I want to keep track of what makes up the connection string
@@ -32,12 +24,12 @@ if (MDB_OPTIONS > "") connectString += "?" + MDB_OPTIONS.join("&");
 
 let connectString = `mongodb+srv://` +
                     `${encodeURIComponent(process.env.MDB_USERNAME)}`     + // auth: username
-                    `:${encodeURIComponent(process.env.MDB_PASS)}`        + // auth: pw, auth token, etc.
+                    `:${encodeURIComponent(process.env.MDB_PASS)}`        + // auth: pw
                     `@${process.env.MDB_APPID}.${process.env.MDB_SERVER}` + // full server name
                     `/${process.env.MDB_DATABASE}`                        + // any "path"
                     "";
 
-console.debug("DEBUG: connectString: " + connectString)
+// console.debug("DEBUG: connectString: " + connectString)
 
 // NEXT: write the function that makes the connection
 //  * async
@@ -47,78 +39,13 @@ console.debug("DEBUG: connectString: " + connectString)
 // DRAFT
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(connectString, { useNewUrlParser: true, useUnifiedTopology: true })
-        console.log(`mongodb connected: ${conn.connection.host}`)
+        const conn = await mongoose.connect(connectString);
+        console.log(`mongodb connected via mongoose: ${conn.connection.host}`);
     } catch (error) {
-        console.log(`DEBUG: DB Connection Failed!`)
-        console.log(`error ${error.message}`)
-    }
-}
-
-// NEXT: export (`export default` or `module.exports`)
-// DRAFT
-export default connectDB;
-
-
-
-// --- examples ----------- -----------
-/*
-import mongoose from "mongoose";
- */
-const CONNECTION_URL =
-  "mongodb+srv://ariestitt:mokube123@cluster0.jozbiey.mongodb.net/memories?retryWrites=true&w=majority&appName=Cluster0";
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(CONNECTION_URL);
-    console.log(`connected to db`)
-  } catch (error) {
-    console.log(`error: ${error.message}`);
-    process.exit(1);
-  }
+        console.error(`Error: database connection failed: ${error.message}`);
+        process.exit(1);
+    };
 };
 
 export default connectDB;
-
-// -----------
-
-if (process.env.MODE != "production" ) { require("dotenv").config(); }
-const mongoose = require('mongoose');
-
-// MongoDB connection elements
-// const REGION="us-east-1.aws"
-const DB_PREFIX = "mongodb+srv"
-const DB_APPNAME = "honeynutcluster"
-const DB_APPID = "71qzsuw"
-const DB_SERVERROOT = "mongodb.net"
-const DB_DATABASE = "fullcrud"
-
-const DB_USERNAME = encodeURIComponent(process.env.MDB_USERNAME)
-const DB_PASSWORD = encodeURIComponent(process.env.MDB_PASSWORD)
-
-const DB_AUTHSTRING = `${DB_USERNAME}:${DB_PASSWORD}`
-const DB_SERVER = [DB_APPNAME, DB_APPID, DB_SERVERROOT].join(".")
-
-const connectString = `${DB_PREFIX}://${DB_AUTHSTRING}@${DB_SERVER}/${DB_DATABASE}`
-
-// console.debug("DEBUG: connectString: " + connectString)
-
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(connectString)
-        console.log(`mongodb connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.log(`DEBUG: DB Connection Failed!`)
-        console.log(`error ${error.message}`)
-    }
-}
-
-// export default connectDB  // not with Common style
-module.exports = connectDB;
-
-/*
-// If we are only making a conneciton one time, how do we access different collections?
-// is this valid syntax?
-let targetCollection = 'test';
-await mongoose.connect(connectString, {'collection': targetCollection});
-*/
+// or `module.exports = connectDB;` with Common style, but shouldn't need that
