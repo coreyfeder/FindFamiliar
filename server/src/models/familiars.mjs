f2fd71935364a5d4bf25306a74482fcbdfc842a7
Corey Feder <corey.feder@gmail.com>
2024-04-23 21:43:45 -0400

    move stats into subdocuments in nested paths

--- server/src/models/familiars.mjs
+++ server/src/models/familiars.mjs
@@ -1,28 +1,29 @@
 import mongoose from "mongoose";
 
+const statSchema = new mongoose.Schema({
+    value: { type: Number, required: true, },
+    note: { type: String, trim: true, },
+})
+
 const familiarSchema = new mongoose.Schema({
     name: { type: String, trim: true, required: true, index: text, },
     description: { type: String, trim: true, required: true, },
     environment: { type: String, trim: true, },
     trust: { type: String, trim: true, },
-    offense: { type: Number, required: true, },
-    offense_note: { type: String, trim: true, },
-    defense: { type: Number, required: true, },
-    defense_note: { type: String, trim: true, },
-    riz: { type: Number, required: true, },
-    riz_note: { type: String, trim: true, },
-    cuddle_charm: { type: Number, required: true, },
-    cuddle_charm_note: { type: String, trim: true, },
-    cuddle_tolerance: { type: Number, required: true, },
-    cuddle_tolerance_note: { type: String, trim: true, },
-    stealth: { type: Number, required: true, },
-    stealth_note: { type: String, trim: true, },
-    intimidation: { type: Number, required: true, },
-    intimidation_note: { type: String, trim: true, },
-    individuality: { type: Number, required: true, },
-    individuality_note: { type: String, trim: true, },
-    peril: { type: Number, required: true, },
-    peril_note: { type: String, trim: true, },
+    stats: {
+        offense: { type: statSchema, default: () => ({}) },
+        defense: { type: statSchema, default: () => ({}) },
+        riz: { type: statSchema, default: () => ({}) },
+        cuddle_charm: { type: statSchema, default: () => ({}) },
+        cuddle_tolerance: { type: statSchema, default: () => ({}) },
+        stealth: { type: statSchema, default: () => ({}) },
+        intimidation: { type: statSchema, default: () => ({}) },
+        individuality: { type: statSchema, default: () => ({}) },
+        peril: { type: statSchema, default: () => ({}) },
+    }
 });
 
-export default mongoose.model(schema=familiarSchema, collection="familiars");
+// const Familiar = mongoose.model(schema=familiarSchema, collection="familiars");
+const Familiar = mongoose.model('Familiar', familiarSchema);
+
+export default Familiar;
